9dfb1e08547eb98b817233cd2966399bb434fb3e
Adnan <adnaa.n.n.001@gmail.com>
2026-02-24 11:41:15 +0530

    fixed header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import Dashboard from './Components/Dashboard';
-import AdminPanel from './Components/AdminPanel';
-import PhysicsTopicsPage from './Components/PhysicsTopicsPage';
-import ChemistryTopicPage from './Components/ChemistryTopicPage';
-import BiologyTopicPages from './Components/BiologyTopicPages';
-import Home from './Pages/Home';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import PracticalExperiment from './Components/PracticalExperiment';
-import FaradaySimulation from './Experiments/FaradaySimulation';
-import { OhmsLawVirtualLab } from './Experiments/OhmsLawVirtualLab';
-import PHVirtuallab from './Experiments/PHVirtualLab';
-import ChemicalReactionsInteractiveLabhem from './Experiments/ChemicalReactionsInteractiveLab';
-import AcidBaseLabPro from './Experiments/AcidBaseLabPro';
-import SoapCleansingActionLab from './Experiments/SoapCleansingActionLab';
-import PhotosynthesisVirtualLab from './Experiments/PhotosynthesisVirtualLab';
-import RespirationLab from './Experiments/RespirationLab';
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
+import Dashboard from "./Components/Dashboard";
+import AdminPanel from "./Components/AdminPanel";
+import PhysicsTopicsPage from "./Components/PhysicsTopicsPage";
+import ChemistryTopicPage from "./Components/ChemistryTopicPage";
+import BiologyTopicPages from "./Components/BiologyTopicPages";
+import Home from "./Pages/Home";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+import PracticalExperiment from "./Components/PracticalExperiment";
+import FaradaySimulation from "./Experiments/FaradaySimulation";
+import { OhmsLawVirtualLab } from "./Experiments/OhmsLawVirtualLab";
+import PHVirtuallab from "./Experiments/PHVirtualLab";
+import ChemicalReactionsInteractiveLabhem from "./Experiments/ChemicalReactionsInteractiveLab";
+import AcidBaseLabPro from "./Experiments/AcidBaseLabPro";
+import SoapCleansingActionLab from "./Experiments/SoapCleansingActionLab";
+import PhotosynthesisVirtualLab from "./Experiments/PhotosynthesisVirtualLab";
+import RespirationLab from "./Experiments/RespirationLab";
 
 function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-black text-white">
       <Header />
+
       <main className="flex-1 w-full">
         <Outlet />
       </main>
+
       <Footer />
     </div>
   );
@@ -36,28 +40,27 @@ export function App() {
     <Router>
       <AuthProvider>
         <Routes>
-          <Route element={<MainLayout/>}/>
+          <Route element={<MainLayout />}>
             <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/admin" element={<AdminPanel />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/admin" element={<AdminPanel />} />
 
             {/* Subject pages */}
             <Route path="/physics" element={<PhysicsTopicsPage />} />
             <Route path="/chemistry" element={<ChemistryTopicPage />} />
             <Route path="/biology" element={<BiologyTopicPages />} />
 
-              {/* Experiment lesson pages */}
-             <Route path="/allexp/:id" element={<PracticalExperiment/>} />
-             <Route path="/faradayLaw" element={<FaradaySimulation/>} />
-             <Route path="/ohmsLaw" element={<OhmsLawVirtualLab />} />
-             <Route path="/PhSeries" element={<PHVirtuallab/>} />
-             <Route path="/chemicalrxn" element={<ChemicalReactionsInteractiveLabhem />} />
-             <Route path="/AcidBase" element={<AcidBaseLabPro/>} />
-             <Route path="/soap" element={<SoapCleansingActionLab/>} />
-             <Route path="/photosynthesis" element={<PhotosynthesisVirtualLab />} />
-             <Route path="/Respiration" element={<RespirationLab/>} />
-             {/* <Route path="/starchtest" element={<StarchTestLab />} /> */}
-            
+            {/* Experiments */}
+            <Route path="/allexp/:id" element={<PracticalExperiment />} />
+            <Route path="/faradayLaw" element={<FaradaySimulation />} />
+            <Route path="/ohmsLaw" element={<OhmsLawVirtualLab />} />
+            <Route path="/PhSeries" element={<PHVirtuallab />} />
+            <Route path="/chemicalrxn" element={<ChemicalReactionsInteractiveLabhem />} />
+            <Route path="/AcidBase" element={<AcidBaseLabPro />} />
+            <Route path="/soap" element={<SoapCleansingActionLab />} />
+            <Route path="/photosynthesis" element={<PhotosynthesisVirtualLab />} />
+            <Route path="/Respiration" element={<RespirationLab />} />
+          </Route>
         </Routes>
       </AuthProvider>
     </Router>
